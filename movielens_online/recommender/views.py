f83dfcd770d03ce668ebff4aa17ad1d54b0bd0f3
Josh Friese <Friese@Friese.local>
2017-03-22 08:42:40 -0400

    started adding views and templates

--- movielens_online/recommender/views.py
+++ movielens_online/recommender/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from models import Movie
 
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def top_20(request):
+    movies = [movies for movies in Movie.object.all()]
+    return render(request, 'recommender/top_20.html', {'question': })
